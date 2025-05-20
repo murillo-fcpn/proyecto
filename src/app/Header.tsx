9e3beeec4a9c016a4a1eff6d62186c0b082d442a
alansanc11 <alansanc11@gmail.com>
2025-05-20 16:23:20 -0400

    Header

---
+++ src/app/Header.tsx
@@ -0,0 +1,17 @@
+import "./header.css"
+
+export function Header (){
+    return(
+        <header className="header">
+                    <h1 className="header-title">Header</h1>
+                    <nav className="header-nav">
+                                        <ul className="header-list">
+                                                <a href="">Home</a>
+                                                <a href="">asdasd</a>
+                                                <a href="">Asdasd</a>
+                                                <a href="">adada</a>
+                                        </ul>
+                    </nav>
+        </header>
+    );
+}
