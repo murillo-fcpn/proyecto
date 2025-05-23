c97f9844c48a5784f43b1b844d89faa85aab0032
Israel Murillo <imurillof@fcpn.edu.bo>
2025-05-23 01:53:27 -0400

    fix: update use client

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,5 +1,4 @@
 "use client";
-
 import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import styles from './Navbar.module.css'; // Importa tus estilos CSS Modules
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,23 @@
 "use client";
-
 import styles from "./page.module.css";
 import Navbar from "./components/Navbar";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 
 export default function Home() {
 
   return (
     <BrowserRouter >
-
       <div className={styles.page}>
         <Navbar />
-        <main className={styles.main}></main>
+        <main className={styles.main}>
+          <Routes>
+            <Route path="/" element={<h1>Inicio</h1>} />
+            <Route path="/acerca" element={<h1>Acerca de</h1>} />
+            <Route path="/contacto" element={<h1>Contacto</h1>} />
+            <Route path="*" element={<h1>Página no encontrada</h1>} />
+          </Routes>
+        </main>
       </div>
     </BrowserRouter >
   );
