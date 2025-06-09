111c9a210a06c4843345196ffcaa861cb2a97a5d
Israel Murillo <imurillof@fcpn.edu.bo>
2025-06-09 13:22:31 -0400

    new configuration for images

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains : ['rickandmortyapi.com'],
+    domains : ['rickandmortyapi.com', '1000logos.net'],
   },
 };
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,24 @@
 import styles from "@/assets/page.module.css";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <div className={styles.wrapper}>
       <div className={styles.background}>
-        <img src="https://1000logos.net/wp-content/uploads/2022/03/Rick-and-Morty.png" alt="rick y morty" height={200} />
+        
+        <Image
+          src={
+            "https://1000logos.net/wp-content/uploads/2022/03/Rick-and-Morty.png"
+          }
+          alt="rick y morty"
+          width={200} 
+          height={200}
+        />
         <p>Trabajo Programacion WEB II </p>
         <p>nuestro equipo G-32</p>
         <p>Integrantes: </p>
         <p>Alan Paulo Sanchez Silva</p>
         <p>Israel Elias Murillo Flores</p>
-
       </div>
       <div></div>
     </div>
